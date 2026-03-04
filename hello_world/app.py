ec8fbe845f745e3ba3072ba516aa37d0e09884d5
RSreenath <sreenathravidra123@gmail.com>
2026-03-05 01:26:31 +0530

    code has been modified

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,37 +1,29 @@
 import json
 import csv
-# import requests
+import boto3
+import io
 
+s3 = boto3.client("s3")
 
 def lambda_handler(event, context):
-
 
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
 
+    print(f"Processing file from bucket: {bucket}, key: {key}")
 
+    response = s3.get_object(Bucket=bucket, Key=key)
+    content = response['Body'].read().decode('utf-8')
 
-    with open("raw_orders.csv", "r") as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            print(row)
+    csv_file = io.StringIO(content)
+    reader = csv.DictReader(csv_file)
 
-    print(bucket)
-    print(key)
-
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
+    for row in reader:
+        print(row)
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
+            "message": "File processed successfully"
         }),
     }
