cd2973d5bebdc897df1b33fe0b5819bf573f9b09
RSreenath <sreenathravidra123@gmail.com>
2026-03-04 22:44:56 +0530

    csv name changed

--- hello_world/app.py
+++ hello_world/app.py
@@ -4,34 +4,14 @@ import csv
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
 
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
 
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
 
 
 
-    with open("employees.csv", "r") as file:
+    with open("raw_orders.csv", "r") as file:
         reader = csv.DictReader(file)
 
     for row in reader:
